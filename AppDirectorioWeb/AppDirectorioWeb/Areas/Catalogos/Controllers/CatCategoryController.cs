eaf01cc7b31cdf02aff52daeb7c28e8b5eec428c
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-03-26 12:25:39 -0600

    se inicia con diseño y prog backend para pantallas de catCatalogos.

---
+++ AppDirectorioWeb/AppDirectorioWeb/Areas/Catalogos/Controllers/CatCategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Repository.IRepository;
+
+namespace AppDirectorioWeb.Areas.Catalogos.Controllers
+{
+    [Area("Catalogos")]
+    public class CatCategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CatCategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+         #region API_CALLS
+
+         [HttpGet]
+         public IActionResult GetAllParents()
+         {
+             var parentsObj = _unitOfWork.Category.GetAll(x => x.IdPadre == 0);
+             return Json(new{data= parentsObj });
+         }
+         #endregion
+    }
+}
--- AppDirectorioWeb/AppDirectorioWeb/Startup.cs
+++ AppDirectorioWeb/AppDirectorioWeb/Startup.cs
@@ -1,5 +1,5 @@
 
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
