44899393506a24f0a05df5fb8438488d2a44860f
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-05-06 11:52:41 -0600

    se ordena codigo en controller de negocios

--- AppDirectorioWeb/AppDirectorioWeb/Areas/Negocios/Controllers/NegociosController.cs
+++ AppDirectorioWeb/AppDirectorioWeb/Areas/Negocios/Controllers/NegociosController.cs
@@ -1,27 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authorization;
-
-
-using Microsoft.AspNetCore.Authorization;
-using System.IO;
+﻿using AutoMapper;
 using DataAccess.Models;
-using Models.ViewModels;
 using DataAccess.Repository.IRepository;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
+using Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 using Utiles;
 
 namespace AppDirectorioWeb.Controllers
@@ -30,19 +25,25 @@ namespace AppDirectorioWeb.Controllers
     public class NegociosController : Controller
     {
         /*private readonly ILogger<HomeController> _logger;*/
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IMapper _mapper;
-        private readonly UserManager<IdentityUser> _userManager;
+
+        #region Private Fields
+
         private readonly IEmailSender _emailSender;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly DirectorioOnlineCoreContext context;
         private readonly IWebHostEnvironment hostingEnvironment;
 
-        private readonly DirectorioOnlineCoreContext context;
+        #endregion Private Fields
+
+        #region Public Constructors
 
         public NegociosController(DirectorioOnlineCoreContext context,
             IHttpContextAccessor httpContextAccessor,
-            IUnitOfWork unitOfWork, 
+            IUnitOfWork unitOfWork,
             IMapper mapper,
             UserManager<IdentityUser> userManager,
             IEmailSender emailSender,
@@ -57,20 +58,23 @@ namespace AppDirectorioWeb.Controllers
             this.hostingEnvironment = hostingEnvironment;
         }
 
-       
+        #endregion Public Constructors
+
+        #region Public Methods
 
         [HttpGet]
         [AllowAnonymous]
-        public  IActionResult AgregarNegocio()
+        public IActionResult AgregarNegocio()
         {
+            var tt = HttpContext.Session.GetString("UserId").ToString();
             AddUpdBusinessViewModel model = new AddUpdBusinessViewModel();
             //Se agrega cataegorias y departamentos
             model.Business = new BussinesViewModel()
             {
-                Categories=_unitOfWork.Category.GetAll().Where(x=>x.IdPadre==1).Select(x=>new { x.Id,x.Nombre}).Select(i=>new SelectListItem 
-                { 
-                Text=i.Nombre,
-                Value=i.Id.ToString()
+                Categories = _unitOfWork.Category.GetAll().Where(x => x.IdPadre == 1).Select(x => new { x.Id, x.Nombre }).Select(i => new SelectListItem
+                {
+                    Text = i.Nombre,
+                    Value = i.Id.ToString()
                 }),
 
                 Departamentos = _unitOfWork.Departament.GetAll().Where(x => x.Activo == true).Select(x => new { x.Id, x.Nombre }).Select(i => new SelectListItem
@@ -82,8 +86,7 @@ namespace AppDirectorioWeb.Controllers
 
             //Se agrega data de días
             var Days = _unitOfWork.Category.GetAll(x => x.IdPadre == 25).ToList();
-           
-            
+
             List<HorarioNegocioViewModel> ScheduleDayList = new List<HorarioNegocioViewModel>();
             foreach (var d in Days)
             {
@@ -106,23 +109,22 @@ namespace AppDirectorioWeb.Controllers
                 feature.IdFeature = fn.Id;
                 feature.Feature = fn.Nombre;
                 feature.CreateDate = DateTime.Now;
-                feature.IdUserCreate   = "0";
+                feature.IdUserCreate = "0";
                 FeatureNegocios.Add(feature);
             }
 
             model.FeatureNegocios = FeatureNegocios;
             return View(model);
         }
-    
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
         public async Task<IActionResult> AgregarNegocio(AddUpdBusinessViewModel model)
         {
+        
             if (ModelState.IsValid)
             {
-               
-
                 var user = new IdentityUser { UserName = model.User.Email, Email = model.User.Email, PhoneNumber = model.User.Telefono };
 
                 var result = await _userManager.CreateAsync(user, model.User.Password);
@@ -140,7 +142,6 @@ namespace AppDirectorioWeb.Controllers
                 await _userManager.AddToRoleAsync(user, SP.Role_BusinesAdmin);
 
                 //guardamos el detalle del usuario.
-           
 
                 var userDetail = new UserDetail { UserId = user.Id, FullName = model.User.FullName, NotificationsPromo = true, RegistrationDate = DateTime.Now, IdUserCreate = idUserCreate };
                 _unitOfWork.UserDetail.Add(userDetail);
@@ -148,117 +149,142 @@ namespace AppDirectorioWeb.Controllers
                 //asignamos el id del usuario a su negocio(idUserCreate)
                 model.Business.IdUserCreate = idUserCreate;
                 model.Business.CreateDate = DateTime.Now;
-                model.Business.IdUserOwner= user.Id;
+                model.Business.IdUserOwner = user.Id;
                 model.Business.Status = 19;
+
                 //logica para logo
-                string uniqueFileName = null;
-                if (model.Logo!=null)
-                {
-                  string uploadsFolder=  Path.Combine(hostingEnvironment.WebRootPath, "ImagesBusiness");
-                  uniqueFileName= Guid.NewGuid().ToString() +  "_logoBusiness_" + model.Logo.FileName;
-                  string filePath=Path.Combine(uploadsFolder,uniqueFileName);
-                  model.Logo.CopyTo(new FileStream(filePath,FileMode.Create));
-                }
-
+                string uniqueFileName = SaveLogoPicture(model);
                 model.Business.LogoNegocio = uniqueFileName;
-                var negocio = _mapper.Map<Negocio>(model.Business);
-           
 
+                var negocio = _mapper.Map<Negocio>(model.Business);
                 _unitOfWork.Business.Add(negocio);
                 _unitOfWork.Save();
 
+                SaveFeaturesBusiness(model, negocio, idUserCreate);
 
-                foreach (var feature in model.FeatureNegocios)
-                {
-                    feature.IdNegocio = negocio.Id;
-                    feature.IdUserCreate = idUserCreate;
-                    feature.CreateDate = DateTime.Now;
-                }
-
-
-                var features = _mapper.Map<List<FeatureNegocio>>(model.FeatureNegocios);
-                _unitOfWork.Feature.InsertList(features);
-
-                foreach (var sche in model.HorarioNegocios)
-                {
-                    sche.IdNegocio = negocio.Id;
-                    sche.IdUserCreate = idUserCreate;
-                    sche.CreateDate = DateTime.Now;
-                }
-
-                var schedules = _mapper.Map<List<HorarioNegocio>>(model.HorarioNegocios);
-                _unitOfWork.ScheduleBusiness.InsertList(schedules);
-
+                SaveSchedulesBusiness(model, negocio, idUserCreate);
 
                 //logica para galerias de imagenes
-                string uniqueFileNames = null;
-                List<ImagenesNegocioViewModel> lstPictures = new List<ImagenesNegocioViewModel>();
-                if (model.PicturesBusiness != null && model.PicturesBusiness.Count>0)
-                {
-                   
-                    foreach (IFormFile picture in model.PicturesBusiness)
-                    {
-                        ImagenesNegocioViewModel pic = new ImagenesNegocioViewModel();
-                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "ImagesBusiness");
-                        uniqueFileNames = Guid.NewGuid().ToString() +  "_picturesBusiness_" + picture.FileName;
-                        string filePath = Path.Combine(uploadsFolder, uniqueFileNames);
-                        picture.CopyTo(new FileStream(filePath, FileMode.Create));
+                SavePicturesBusiness(model, negocio, idUserCreate);
 
-                        pic.IdNegocio = negocio.Id;
-                        pic.IdUserCreate = idUserCreate;
-                        pic.CreateDate = DateTime.Now;
-                        pic.Image = uniqueFileNames;
-                        lstPictures.Add(pic);
-                    }
-
-                    var picturesBusiness = _mapper.Map<List<ImagenesNegocio>>(lstPictures);
-                    _unitOfWork.ImageBusiness.InsertList(picturesBusiness);
-
-                }
                 _unitOfWork.Save();
 
                 //codigo para envio de correo de verificación de cuenta
-                string returnUrl = null;
-                returnUrl ??= Url.Content("~/");
                 var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-
-                string FilePath = Directory.GetCurrentDirectory() + "\\wwwroot\\EmailTemplates\\TemplateConfirmEmail.html";
-                StreamReader str = new StreamReader(FilePath);
-                string MailText = str.ReadToEnd();
-                str.Close();
-
-                MailText = MailText.Replace("[username]", user.Email).Replace("[linkRef]", HtmlEncoder.Default.Encode(callbackUrl));
-
-               await  _emailSender.SendEmailAsync(user.Email, "Verificación de Cuenta", MailText);
+                string MailText = GetEmailActivationUserAdminBusiness(user, code);
+                await _emailSender.SendEmailAsync(user.Email, "Verificación de Cuenta", MailText);
 
                 //
-
 
                 return RedirectToAction(nameof(ConfirmationBusinessRegistration));
             }
 
-          
-
             return View(model);
-        }
-        [AllowAnonymous]
-        public async Task<IActionResult> GetDetailByBussinesId(int id)
-        {
-        
-            return View();
         }
 
         [AllowAnonymous]
         public IActionResult ConfirmationBusinessRegistration()
         {
             return View();
-
         }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> GetDetailByBussinesId(int id)
+        {
+            return View();
+        }
+
+        #endregion Public Methods
+
+        #region MetodosAuxiliares
+
+        public string GetEmailActivationUserAdminBusiness(IdentityUser user, string codeG)
+        {
+            string MailText = "";
+            string returnUrl = null;
+            returnUrl ??= Url.Content("~/");
+            var code = codeG;
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                protocol: Request.Scheme);
+
+            string FilePath = Directory.GetCurrentDirectory() + "\\wwwroot\\EmailTemplates\\TemplateConfirmEmail.html";
+            StreamReader str = new StreamReader(FilePath);
+            MailText = str.ReadToEnd();
+            str.Close();
+
+            return MailText = MailText.Replace("[username]", user.Email).Replace("[linkRef]", HtmlEncoder.Default.Encode(callbackUrl));
+        }
+
+        public void SaveFeaturesBusiness(AddUpdBusinessViewModel model, Negocio negocio, string idUserCreate)
+        {
+            foreach (var feature in model.FeatureNegocios)
+            {
+                feature.IdNegocio = negocio.Id;
+                feature.IdUserCreate = idUserCreate;
+                feature.CreateDate = DateTime.Now;
+            }
+
+            var features = _mapper.Map<List<FeatureNegocio>>(model.FeatureNegocios);
+            _unitOfWork.Feature.InsertList(features);
+        }
+
+        public string SaveLogoPicture(AddUpdBusinessViewModel model)
+        {
+            string uniqueFileName = "";
+            if (model.Logo != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "ImagesBusiness");
+                uniqueFileName = Guid.NewGuid().ToString() + "_logoBusiness_" + model.Logo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                model.Logo.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            return uniqueFileName;
+        }
+
+        public void SavePicturesBusiness(AddUpdBusinessViewModel model, Negocio negocio, string idUserCreate)
+        {
+            string uniqueFileNames = null;
+            List<ImagenesNegocioViewModel> lstPictures = new List<ImagenesNegocioViewModel>();
+            if (model.PicturesBusiness != null && model.PicturesBusiness.Count > 0)
+            {
+                foreach (IFormFile picture in model.PicturesBusiness)
+                {
+                    ImagenesNegocioViewModel pic = new ImagenesNegocioViewModel();
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "ImagesBusiness");
+                    uniqueFileNames = Guid.NewGuid().ToString() + "_picturesBusiness_" + picture.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileNames);
+                    picture.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                    pic.IdNegocio = negocio.Id;
+                    pic.IdUserCreate = idUserCreate;
+                    pic.CreateDate = DateTime.Now;
+                    pic.Image = uniqueFileNames;
+                    lstPictures.Add(pic);
+                }
+
+                var picturesBusiness = _mapper.Map<List<ImagenesNegocio>>(lstPictures);
+                _unitOfWork.ImageBusiness.InsertList(picturesBusiness);
+            }
+        }
+
+        public void SaveSchedulesBusiness(AddUpdBusinessViewModel model, Negocio negocio, string idUserCreate)
+        {
+            foreach (var sche in model.HorarioNegocios)
+            {
+                sche.IdNegocio = negocio.Id;
+                sche.IdUserCreate = idUserCreate;
+                sche.CreateDate = DateTime.Now;
+            }
+
+            var schedules = _mapper.Map<List<HorarioNegocio>>(model.HorarioNegocios);
+            _unitOfWork.ScheduleBusiness.InsertList(schedules);
+        }
+
+        #endregion MetodosAuxiliares
     }
 }
