e22aec9811b6819ad47b6d290bc7597e6d0e5372
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-05-18 15:47:36 -0600

    mejora en cargar reviews

--- AppDirectorioWeb/AppDirectorioWeb/wwwroot/js/Handlers/ReviewsHandler.js
+++ AppDirectorioWeb/AppDirectorioWeb/wwwroot/js/Handlers/ReviewsHandler.js
@@ -3,7 +3,7 @@
 });
 
 function loadReviews() {
-    console.log($('#IBusinessId').val());
+  
     var BusinessId = $('#IBusinessId').val();
 
     $.ajax({
@@ -12,20 +12,26 @@ function loadReviews() {
         success: function (data) {
 
             $('#revContador').text('Reseñas (' + data.reviewsObj.length+')');
+            $('#spanReviewCount').text('(' + data.reviewsObj.length + ' Reseñas)');
+
+            var totalRev = data.reviewsObj.length;
+            var sumStars = 0;
 
             $.each(data.reviewsObj, function (index, val) {
-             
+
                 var starsString = '';
                 for (var i = 0; i < this.stars; i++) {
-                    starsString += '  <li class="list-inline-item">'+
-                        '<i class="fa fa-star" aria-hidden="true" ></i>'+
-                                    '</li>';
+                    starsString += '  <li class="list-inline-item">' +
+                        '<i class="fa fa-star" aria-hidden="true" ></i>' +
+                        '</li>';
                 }
+
+                sumStars += this.stars;
 
                 $("#divContentMainReview").append(
                     ' <div class="media mb-3">' +
                     '<div class="media-img">' +
-                    '<img style="width: 50px;" src="/ImagesBusiness/' + this.pictureUser + '" data-src="/ImagesBusiness/' + this.pictureUser+'" class="mr-3 media-object rounded-circle " alt="Image User">' +
+                    '<img style="width: 50px;" src="/ImagesBusiness/' + this.pictureUser + '" data-src="/ImagesBusiness/' + this.pictureUser + '" class="mr-3 media-object rounded-circle " alt="Image User">' +
                     '   </div>' +
                     '<div class="media-body">' +
                     ' <h5 class="media-heading">' + this.userNameComments + '</h5>' +
@@ -39,9 +45,23 @@ function loadReviews() {
                     '</div>' +
                     '  <br/>'
                 )
-            })
+            });
+
+            var promReviewStars = float2int(sumStars / totalRev) ;
+            
+            for (var i = 0; i < promReviewStars; i++) {
+                $("#mainStarsUl").append('  <li class="list-inline-item">' +
+                    '<i class="fa fa-star" aria-hidden="true" ></i>' +
+                    '</li>');
+            }
+           
         }
     });
 
 }
+
+function float2int(value) {
+    return value | 0;
+}
+
 
