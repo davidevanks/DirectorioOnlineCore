78e1b79fa5f8731ec4b73281fe91e8e9394a28a5
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-06-08 16:27:00 -0600

    BD updated

--- AppDirectorioWeb/DirectorioOnlineCoreDb/bs/Tables/Reviews.sql
+++ AppDirectorioWeb/DirectorioOnlineCoreDb/bs/Tables/Reviews.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [bs].[Reviews] (
-    [Id]         BIGINT         IDENTITY (1, 1) NOT NULL,
-    [IdBusiness] INT            NOT NULL,
-    [IdUser]     NVARCHAR (450) NOT NULL,
-    [FullName]   VARCHAR (500)  NULL,
-    [Comments]   VARCHAR (MAX)  NULL,
-    [Stars]      INT            NOT NULL,
-    [Active]     BIT            NOT NULL,
+    [Id]           BIGINT         IDENTITY (1, 1) NOT NULL,
+    [IdBusiness]   INT            NOT NULL,
+    [IdUser]       NVARCHAR (450) NOT NULL,
+    [FullName]     VARCHAR (500)  NULL,
+    [Comments]     VARCHAR (MAX)  NULL,
+    [EmailComment] VARCHAR (300)  NULL,
+    [Stars]        INT            NOT NULL,
+    [Active]       BIT            NOT NULL,
     CONSTRAINT [PK_Reviews] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Reviews_Negocio] FOREIGN KEY ([IdBusiness]) REFERENCES [bs].[Negocio] ([Id])
 );
+
+
 
 
 
