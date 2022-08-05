80e0e686457471e00f6bc8510a7fb5d9c19c39e9
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-08-05 16:08:09 -0600

    se creo index(pantalla catalogo) para cupones. tambien se agrego al menú

--- AppDirectorioWeb/AppDirectorioWeb/Areas/Cuponera/Controllers/CuponeraController.cs
+++ AppDirectorioWeb/AppDirectorioWeb/Areas/Cuponera/Controllers/CuponeraController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Utiles;
 
 namespace AppDirectorioWeb.Areas.Cuponera.Controllers
 {
     [Area("Cuponera")]
     public class CuponeraController : Controller
     {
+        [Authorize(Roles = SP.Role_BusinesAdmin + "," + SP.Role_Admin)]
         public IActionResult Index()
         {
             return View();
