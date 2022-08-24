1ddab9e97099d7c4e31e5a0aeeaa64be0ecbee2a
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2022-08-24 11:40:17 -0600

    se realizaron tablas para el inicio de desarrollo de catologos de producto/servicio

---
+++ AppDirectorioWeb/DirectorioOnlineCoreDb/bs/Tables/ItemCatalogo.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [bs].[ItemCatalogo] (
+    [Id]                     INT             NOT NULL,
+    [IdConfigCatalogo]       INT             NULL,
+    [CodigoItemRef]          VARCHAR (MAX)   NOT NULL,
+    [NombreItem]             VARCHAR (MAX)   NOT NULL,
+    [DescripcionItem]        VARCHAR (MAX)   NOT NULL,
+    [PrecioUnitario]         DECIMAL (18, 2) NOT NULL,
+    [TieneDescuento]         BIT             NULL,
+    [PorcentajeDescuento]    INT             NULL,
+    [ImagenItem]             VARCHAR (MAX)   NOT NULL,
+    [Activo]                 BIT             NULL,
+    [FechaCreacion]          DATETIME        NOT NULL,
+    [IdUsuarioCreacion]      NVARCHAR (500)  NOT NULL,
+    [FechaActualizacion]     DATETIME        NULL,
+    [IdUsuarioActualizacion] NVARCHAR (500)  NULL,
+    CONSTRAINT [PK_ItemCatalogo] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ItemCatalogo_ConfigCatalogo] FOREIGN KEY ([IdConfigCatalogo]) REFERENCES [bs].[ConfigCatalogo] ([Id])
+);
+
