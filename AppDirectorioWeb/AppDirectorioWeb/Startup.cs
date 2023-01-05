4d7b06878bca1aaf05d8814bd307e7895f95e845
kenneth.gaitan@peoplethrust.com <kenneth.gaitan@peoplethrust.com>
2023-01-05 16:38:40 -0600

    ultimos ajustes

--- AppDirectorioWeb/AppDirectorioWeb/Startup.cs
+++ AppDirectorioWeb/AppDirectorioWeb/Startup.cs
@@ -14,6 +14,7 @@ using System;
 using NLog;
 using Utiles;
 using NLog.Extensions.Logging;
+using System.Web.Mvc;
 
 namespace AppDirectorioWeb
 {
@@ -65,6 +66,7 @@ namespace AppDirectorioWeb
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{area=Home}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
 
